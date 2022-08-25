aa402abe75f7312289d861876000aad560f35823
kome44733 <110373715+kome44733@users.noreply.github.com>
2022-08-25 04:52:25 +0000

    [Add]いいねコメント

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   def show
     @new_book = Book.new
     @book = Book.find(params[:id])
+    @book_comment = BookComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+   
   end
 
   def create
